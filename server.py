56e30f8b2d04abf0d468bcb7b64ad3cad2be4afb
Matthias Bothe <matthias.bothe@mb.speedport.ip>
2020-02-26 22:34:42 +0100

    Added Restaurant Object

--- server.py
+++ server.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
+from jinja2 import Template
 
 path = "/Users/matthias.bothe/code/foundations/foundations-restaurants/restaurants.txt"
 data = open(path, "r")
-content = data.read()
+all_restaurants = data.read()
 
-def generate_html(content):
-    print("""
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Success!</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/style.css">
-  </head>  
-  <body style="text-align:left">
-    <h1>Restaurant list</h1>
-    <p>{}</p>
-  </body>
-</html>
-""".format(content)
-    )
+class Restaurant:
+
+    def __init__(self, name, location):
+
+        self.name = name
+        self.location = location
+
+    def getName(self):
+        return self.name
+
+    def getLocation(self):
+        return self.location
+
+for restaurant in all_restaurants:
+    restaurant = Restaurant()
+
+    tm = Template("The name is {{ per.getName() }} and it is located in {{ per.getLocation() }}")
+    msg = tm.render(per=restaurant)
+    print(msg)
 
 data.close()
